b55be660527d70861f26411c07a238d15080dd1c
nonodamiens <arnaud.mercier80@gmail.com>
2020-04-12 17:12:06 +0200

    Make player moving capacities and death to walls

--- snake.py
+++ snake.py
@@ -1,5 +1,7 @@
 import random
 import os
+import time
+from msvcrt import getch, kbhit
 # os.system("mode con: cols=25 lines=25")
 
 clear = lambda: os.system('cls') #on Windows System
@@ -9,7 +11,17 @@ head = "☺"
 corps = "■"
 pastille = "●"
 
-area = {"0" : "         ",\
+j_coord = (4, 4)
+p_coord = (random.randint(0, 9), random.randint(0, 9))
+while p_coord == j_coord:
+	p_coord = (random.randint(0, 9), random.randint(0, 9))
+go = True
+d = 'down'
+x = ""
+while go:	
+	clear()
+
+	area = {"0" : "         ",\
 		"1" : "         ",\
 		"2" : "         ",\
 		"3" : "         ",\
@@ -20,22 +32,6 @@ area = {"0" : "         ",\
 		"8" : "         ",\
 		}
 
-j_coord = (4, 4)
-p_coord = (random.randint(0, 9), random.randint(0, 9))
-while p_coord == j_coord:
-	p_coord = (random.randint(0, 9), random.randint(0, 9))
-
-print("pastille coord: ", p_coord)
-area[str(p_coord[0])] = " " * (p_coord[1] - 1) + pastille + " " * (9 - p_coord[1])
-area[str(j_coord[0])] = area[str(j_coord[0])][:j_coord[1]] + head + area[str(j_coord[0])][j_coord[1] + 1:]
-
-print("┌─────────┐\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n└─────────┘"\
-	.format(area["0"], area["1"], area["2"], area["3"], area["4"], area["5"], area["6"], area["7"], area["8"]))
-print("coord joueur:", j_coord)
-
-go = True
-while go:
-	clear()
 	print("pastille coord: ", p_coord)
 	area[str(p_coord[0])] = " " * (p_coord[1] - 1) + pastille + " " * (9 - p_coord[1])
 	area[str(j_coord[0])] = area[str(j_coord[0])][:j_coord[1]] + head + area[str(j_coord[0])][j_coord[1] + 1:]
@@ -43,36 +39,31 @@ while go:
 	print("┌─────────┐\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n└─────────┘"\
 		.format(area["0"], area["1"], area["2"], area["3"], area["4"], area["5"], area["6"], area["7"], area["8"]))
 	print("coord joueur:", j_coord)
-	
-	x = raw_input()
-	if x == "e":
+	time.sleep(0.5)
+	if kbhit():
+		x = getch()
+	if x == b'\x00':
+		d = {b"H": "up", b"P": "down", b"M": "right", b"K": "left"}[getch()]
+	elif x == b"q":
 		go = False
-	elif x == "q":
-		area[str(j_coord[0])] = "         "
+	if d == "up":
+		j_coord = (j_coord[0] - 1, j_coord[1])
+	if d == "down":
+		j_coord = (j_coord[0] + 1, j_coord[1])
+	if d == "right":
+		j_coord = (j_coord[0], j_coord[1] + 1)
+	if d == "left":
 		j_coord = (j_coord[0], j_coord[1] - 1)
-		area[str(j_coord[0])] = area[str(j_coord[0])][:j_coord[1]] + head + area[str(j_coord[0])][j_coord[1] + 1:]
-	elif x == "s":
-		go = False
-	elif x == "z":
-		go = False
-	elif x == "w":
-		go = False
+	
+	if -1 in j_coord or 9 in j_coord:
+		print('\033[93myou died\033[0m')
+		print("restart ? (y)")
+		r = input()
+		if r == "y":
+			j_coord = (4,4)
+			d = 'down'
+			x = ''
+		else :
+			go = False
+	x = ''
 
-
-
-# print(area)
-# clear()
-# print(area)
-
-# area[5][5] = corps
-# coord = (5, 5)
-
-# direction = "b"
-# x = 0
-# while x < 10:
-#   if direction == "b":
-#     area[coord[0]][coord[1]] = " "
-#     coord = (coord[0] + 1, coord[1])
-#     area[coord[0]][coord[1]] = corps
-#     print(area)
-#   x += 1

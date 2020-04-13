51984cad581e9bba12dcf88a5747af55558d95f0
nonodamiens <arnaud.mercier80@gmail.com>
2020-04-13 09:45:10 +0200

    Add a short tail

--- snake.py
+++ snake.py
@@ -8,10 +8,11 @@ clear = lambda: os.system('cls') #on Windows System
 clear()
 
 head = "☺"
-corps = "■"
+tail = "■"
 pastille = "●"
 
 j_coord = (4, 4)
+t_coord =[(3, 4)]
 p_coord = (random.randint(0, 9), random.randint(0, 9))
 while p_coord == j_coord:
 	p_coord = (random.randint(0, 9), random.randint(0, 9))
@@ -19,6 +20,7 @@ go = True
 d = 'down'
 x = ""
 points = 0
+die = False
 
 while go:	
 	clear()
@@ -37,10 +39,10 @@ while go:
 	print("pastille coord: ", p_coord)
 	area[str(p_coord[0])] = area[str(p_coord[0])][:p_coord[1]] + pastille + area[str(p_coord[0])][p_coord[1] + 1:]
 	area[str(j_coord[0])] = area[str(j_coord[0])][:j_coord[1]] + head + area[str(j_coord[0])][j_coord[1] + 1:]
-
+	for c in t_coord:
+		area[str(c[0])] = area[str(c[0])][:c[1]] + tail + area[str(c[0])][c[1] + 1:]
 	print("┌─────────┐\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n│{}│\n└─────────┘"\
 		.format(area["0"], area["1"], area["2"], area["3"], area["4"], area["5"], area["6"], area["7"], area["8"]))
-	print("pastille:", p_coord)
 	print("coord joueur:", j_coord)
 	print("score:", points)
 
@@ -51,24 +53,31 @@ while go:
 		d = {b"H": "up", b"P": "down", b"M": "right", b"K": "left"}[getch()]
 	elif x == b"q":
 		go = False
-	if d == "up":
+	if d == "up" and t_coord[0] != (j_coord[0] - 1, j_coord[1]):
+		t_coord[0] = j_coord
 		j_coord = (j_coord[0] - 1, j_coord[1])
-	if d == "down":
+	elif d == "down" and t_coord[0] != (j_coord[0] + 1, j_coord[1]):
+		t_coord[0] = j_coord
 		j_coord = (j_coord[0] + 1, j_coord[1])
-	if d == "right":
+	elif d == "right" and t_coord[0] != (j_coord[0], j_coord[1] + 1):
+		t_coord[0] = j_coord
 		j_coord = (j_coord[0], j_coord[1] + 1)
-	if d == "left":
+	elif d == "left" and t_coord[0] != (j_coord[0], j_coord[1] - 1):
+		t_coord[0] = j_coord
 		j_coord = (j_coord[0], j_coord[1] - 1)
-	
-	if -1 in j_coord or 9 in j_coord:
+	else:
+		die = True
+	if -1 in j_coord or 9 in j_coord or die == True:
 		print('\033[93myou died\033[0m')
 		print("restart ? (y)")
 		r = input()
 		if r == "y":
 			j_coord = (4,4)
+			t_coord = [(3, 4)]
 			d = 'down'
 			x = ''
 			points = 0
+			die = False
 		else :
 			go = False
 	elif j_coord == p_coord:

6e22aca9cf3fa015088eede1e093862bff4153e6
nonodamiens <arnaud.mercier80@gmail.com>
2020-04-19 15:57:22 +0200

    Add movements to character

--- snake0.0.1.py
+++ snake0.0.1.py
@@ -14,9 +14,31 @@ fond = pygame.image.load("background.png").convert()
 # affichage du fond dans la fenetre
 fenetre.blit(fond, (0, 0))
 
-# Ajout d'un personnage
+# Ajout d'un personnage et enregistrement position
 perso = pygame.image.load("perso.png").convert_alpha()
-fenetre.blit(perso, (200, 300))
+position_perso = perso.get_rect(center=(300, 200))
+fenetre.blit(perso, position_perso)
 
 # rafraichissement de la fenetre pour afficher l'image de fond
 pygame.display.flip()
+
+# Répétition si touche enfoncé
+pygame.key.set_repeat(400, 30)
+
+continuer = 1
+while continuer:
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            continuer = 0
+        if event.type == KEYDOWN:
+            if event.key == K_RIGHT:
+                position_perso = position_perso.move(3, 0)
+            if event.key == K_LEFT:
+                position_perso = position_perso.move(-3, 0)
+            if event.key == K_DOWN:
+                position_perso = position_perso.move(0, 3)
+            if event.key == K_UP:
+                position_perso = position_perso.move(0, -3)
+    fenetre.blit(fond, (0, 0))
+    fenetre.blit(perso, position_perso)
+    pygame.display.flip()

55a3eff2ba08f0dfceadc027a8fd82106a1a27a9
nonodamiens <arnaud.mercier80@gmail.com>
2020-04-25 15:09:00 +0200

    Make the tail growing when snake eat apple

--- snake0.0.1.py
+++ snake0.0.1.py
@@ -19,7 +19,9 @@ snake_speed = 500
 snake_move = USEREVENT + 1
 pygame.time.set_timer(snake_move, snake_speed)
 snake_tail = pygame.draw.rect(fenetre, [0, 0, 200], (40, 30, 10, 10))
-snake_tail_positions = [snake_tail]
+snake_tail_2 = snake_tail.copy()
+snake_tail_2.move_ip(0, -10)
+snake_tail_positions = [snake_tail, snake_tail_2]
 
 # Création d'un sprite pomme
 apple_x = random.randint(0, 9) * 10
@@ -29,6 +31,7 @@ while apple_x == 40:
 while apple_y == 40:
     apple_y = random.randint(0, 9) * 10
 apple = pygame.draw.rect(fenetre, [250, 0, 0], (apple_x, apple_y, 10, 10))
+apple_ate = False
 
 # print des infos
 print(pygame.display.Info())
@@ -51,18 +54,35 @@ while continuer:
                 snake_direction = "U"
         if event.type == snake_move:
             if snake_direction == "R"  and snake_head.x < 90:
-                snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
+                if apple_ate:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions
+                    apple_ate = False
+                else:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
                 snake_head.move_ip(10, 0)
             if snake_direction == "L"  and snake_head.x > 0:
-                snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
+                if apple_ate:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions
+                    apple_ate = False
+                else:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
                 snake_head.move_ip(-10, 0)
             if snake_direction == "D"  and snake_head.y < 90:
-                snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
+                if apple_ate:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions
+                    apple_ate = False
+                else:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
                 snake_head.move_ip(0, 10)
             if snake_direction == "U"  and snake_head.y > 0:
-                snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
+                if apple_ate:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions
+                    apple_ate = False
+                else:
+                    snake_tail_positions = [snake_head.copy()] + snake_tail_positions[:-1]
                 snake_head.move_ip(0, -10)
         if snake_head.x == apple.x and snake_head.y == apple.y:
+            apple_ate = True
             while apple.x == snake_head.x:
                 apple.x = random.randint(0, 9) * 10
             while apple.y == snake_head.y:
@@ -70,7 +90,7 @@ while continuer:
         
     fenetre.fill([0,250,0]) # 'clear' the surface
     pygame.draw.rect(fenetre, (250, 0, 0), apple) # draw the apple
-    for tail in snake_tail_positions:
+    for tail in snake_tail_positions[:-1]:
         print(tail)
         pygame.draw.rect(fenetre, (0, 0, 200), tail)
     pygame.draw.rect(fenetre, (0, 0, 250), snake_head) # draw the new rectangle
